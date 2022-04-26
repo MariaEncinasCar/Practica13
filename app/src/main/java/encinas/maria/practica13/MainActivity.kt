c85c3c55da31b01164a9266f608cef2e60bd4426
MariaEncinasCar <90160554+MariaEncinasCar@users.noreply.github.com>
2022-04-26 10:39:15 -0700

    Initial commit

---
+++ app/src/main/java/encinas/maria/practica13/MainActivity.kt
@@ -0,0 +1,25 @@
+package encinas.maria.practica13
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.ktx.auth
+import com.google.firebase.ktx.Firebase
+import encinas.maria.practica13.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var auth: FirebaseAuth
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // Initialize Firebase Auth
+        auth = Firebase.auth
+
+    }
+
+}
